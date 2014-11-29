626d2d1d1d5626c7276256419e4ee86b9a1b20f0
HTML Test <struev.vs@gmail.com>
2014-11-29 18:52:00 +0300

    task 1217 part 7
    02_services_01level.html
    02_services_02level.html
    03_projects_inside.html

--- js/tools.js
+++ js/tools.js
@@ -342,6 +342,21 @@ var timerSlider     = null;
             }
         });
 
+        $('.video-list .articles-item-img a, .video-list .articles-item-name a').fancybox({
+            padding : 20,
+            helpers : {
+                media : true,
+                title : {
+                    type : 'inside'
+                }
+            },
+            tpl : {
+                closeBtn : '<a title="Закрыть" class="fancybox-item fancybox-close" href="javascript:;"></a>',
+                next     : '<a title="Следующая" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
+                prev     : '<a title="Предыдущая" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
+            }
+        });
+
     });
 
     $(window).bind('load resize', function() {
@@ -480,21 +495,6 @@ var timerSlider     = null;
 
                 curItem.css({'min-height': curHeight});
                 prevItem.css({'min-height': curHeight});
-            }
-        });
-
-        $('.video-preview a, .video-name a').fancybox({
-            padding : 20,
-            helpers : {
-                media : true,
-                title : {
-                    type : 'inside'
-                }
-            },
-            tpl : {
-                closeBtn : '<a title="Закрыть" class="fancybox-item fancybox-close" href="javascript:;"></a>',
-                next     : '<a title="Следующая" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
-                prev     : '<a title="Предыдущая" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
             }
         });
 
